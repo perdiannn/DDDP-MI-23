6d4bcf43e50e6bf2cd5d3ca750356cc90844dfaa
perdiannn <ferdi4nn@gmail.com>
2023-09-29 08:46:37 +0700

    Percabangan

---
+++ 03 Percabangan/if1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int bilangan;
+    cin >> bilangan;
+    if (bilangan == 50){
+        cout << "SETENGAH" << endl;
+    }
+}
---
+++ 03 Percabangan/if2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int nilai;
+    cin >> nilai;
+    if (nilai == 100) {
+        cout << "SEMPURNA" << endl;
+    } else {
+        cout << "BELUM SEMPURNA" << endl;
+    }
+}
